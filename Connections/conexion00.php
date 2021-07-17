56c2819a49e21eb40aa010a5133aba53ac8a461d
JESUS23DJ <53622750+JESUS23DJ@users.noreply.github.com>
2021-07-17 18:42:22 -0500

    Add files via upload

---
+++ Connections/conexion00.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostname00 = "mysql";
+
+$username00 = "i6bm";
+
+$password00 = "i6bmI6BM*#202101";
+
+$database00 = "i6bm";
+
+$conexion00 = mysql_connect ( $hostname00, $username00, $password00 );
+
+mysql_select_db ( $database00, $conexion00 );
+
+?>
---
+++ Connections/conexionUP.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostnameUP = "mysql";
+
+$usernameUP = "i6bm";
+
+$passwordUP = "i6bmI6BM*#202101";
+
+$databaseUP = "i6bm";
+
+$conexionUP = mysql_pconnect ( $hostnameUP, $usernameUP, $passwordUP );
+
+mysql_select_db ( $databaseUP, $conexionUP );
+
+?>
